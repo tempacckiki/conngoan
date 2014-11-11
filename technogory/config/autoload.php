115df1d202112b66baf5b2d25b2f7826892461bb
lytk <trankly@gmail.com>
2014-11-11 23:22:33 +0700

    load helper lib

--- technogory/config/autoload.php
+++ technogory/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session','database','form_validation','templates','esset','modules','vdb','counter','vcart','pagination');
+$autoload['libraries'] = array('session','database','form_validation','templates','esset','modules','vdb','counter','vcart','pagination','helper');
 
 
 /*
